a003eb9aa71c2f107e38f83999f9f93755f1edbb
Riyaaz Dindar <riyaazd@icloud.com>
2025-06-20 16:34:41 -0400

    Implement renderTasks function

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const formValidation = () => {
         taskValidationError.textContent = "";
         saveTask();
         resetForm();
-        // renderTasks();
+        renderTasks();
     }
 };
 
@@ -47,3 +47,61 @@ const resetForm = () => {
     taskDueDate.value = "";
     taskDescription.value = "";
 }
+
+// Render tasks
+const renderTasks = () => {
+    taskList.innerHTML = "";
+    tasksArray.forEach((task, index) => {
+        const taskDiv = document.createElement('div');
+        taskDiv.id = index;
+        taskDiv.classList.add('bg-gradient-to-tr', 'from-blue-200', 'via-indigo-200', 'to-blue-200', 'rounded-lg', 'border-2', 'border-indigo-700', 'p-1');
+
+        const taskTitleSpan = document.createElement('span');
+        taskTitleSpan.textContent = task.taskTitle;
+        taskTitleSpan.classList.add('font-semibold', 'text-indigo-950', 'uppercase');
+
+        const taskDueDateSpan = document.createElement('span');
+        taskDueDateSpan.textContent = task.taskDueDate;
+        taskDueDateSpan.classList.add('font-light', 'text-sm', 'text-slate-400', 'px-1');
+
+        const taskDescriptionP = document.createElement('p');
+        taskDescriptionP.textContent = task.taskDescription;
+        taskDescriptionP.classList.add('font-light', 'text-sm', 'px-1');
+
+        const optionsSpan = document.createElement('span');
+        const editIcon = document.createElement('i');
+        editIcon.classList.add('fas', 'fa-edit', 'text-indigo-900');
+        editIcon.setAttribute('data-te-toggle', 'modal');
+        editIcon.setAttribute('data-te-target', '#form');
+        const deleteIcon = document.createElement('i');
+        deleteIcon.classList.add('fas', 'fa-trash', 'text-indigo-900');
+
+        editIcon.onclick = function() {
+            console.log("Edit button clicked for task at index:", taskDiv.id);
+        }
+
+        deleteIcon.onclick = function() {
+            console.log("Delete button clicked for task at index:", taskDiv.id);
+        }
+
+        // Append icons to optionsSpan
+        optionsSpan.appendChild(editIcon);
+        optionsSpan.appendChild(deleteIcon);
+
+        // Append taskTitleSpan, taskDueDateSpan, taskDescriptionP, optionsSpan to taskDiv
+        taskDiv.appendChild(taskTitleSpan);
+        taskDiv.appendChild(taskDueDateSpan);
+        taskDiv.appendChild(taskDescriptionP);
+        taskDiv.appendChild(optionsSpan);
+
+        // Append taskDiv to tasksList
+        taskList.appendChild(taskDiv);
+    })
+};
+
+// Load tasks from localStorage and render
+(() => {
+    tasksArray = JSON.parse(localStorage.getItem("tasksArray")) || [];
+    console.log("tasksArray");
+    renderTasks();
+})();
